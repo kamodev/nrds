1d2343e550b6623d552ddc880c8d90b3de3ac1ab
Jeremiah N0KMO <n0kmo@n0kmo.com>
2025-08-15 20:58:49 -0600

    Update the Styles.css

--- front-page.php
+++ front-page.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 <main id="main">
-    <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post(); ?>
-                <?php the_content(); ?>
+    <?php if (have_posts()) :?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
         <?php endwhile; ?>
-        <?php else : ?>
-            <p>No content found</p>
+    <?php else : ?>
+        <p>No content found</p>
     <?php endif; ?>
 </main>
 <?php get_footer(); ?>
