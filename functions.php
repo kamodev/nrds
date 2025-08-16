859b8e71b2121689d469d6fef0ef47a2946cff7c
Jeremiah N0KMO <n0kmo@n0kmo.com>
2025-08-16 01:38:43 -0600

    Moved the theme settings to the inludes folder

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+// All functions related to the theme should be added here
+// This file is automatically loaded by WordPress when the theme is activated
+require_once('inc/theme-settings.php');
+
 function nrds_theme_setup() {
     // Add support for various WordPress features
     add_theme_support('title-tag');
@@ -134,100 +138,6 @@ function nrds_custom_logo_setup() {
 }
 add_action('after_setup_theme', 'nrds_custom_logo_setup');
 
-// Theme settings page in admin dashboard
-function nrds_theme_settings_page_menu() {
-    // Add a new menu item under "Appearance" for theme settings page
 
-    add_theme_page(
-        __('Theme Settings', 'nrds-theme'),
-        __('Theme Settings', 'nrds-theme'),
-        'manage_options',
-        'nrds-theme-settings',
-        'nrds_theme_settings_page',
-    );
-}
-add_action('admin_menu', 'nrds_theme_settings_page_menu');
-
-function nrds_theme_settings_init() {
-    // Register a new setting for theme settings page
-    register_setting('nrds_theme_settings', 'nrds_theme_settings_options');
-
-    // Add a new section in the theme settings page
-    add_settings_section(
-        'nrds_theme_settings_general_section',
-        __('General Settings', 'nrds-theme'),
-        'nrds_theme_settings_general_section',
-        'nrds-theme-settings'
-    );
-
-    add_settings_section(
-        'nrds_theme_settings_display_section',
-        __('Display Settings', 'nrds-theme'),
-        'nrds_theme_settings_display_section',
-        'nrds-theme-settings'
-    ); 
-
-    // Add a new field in the section
-    add_settings_field(
-        'nrds_theme_setting_example',
-        __('Example Setting', 'nrds-theme'),
-        'nrds_theme_setting_example_callback',
-        'nrds-theme-settings',
-        'nrds_theme_settings_section'
-    );
-}
-add_action('admin_init', 'nrds_theme_settings_init');
-
-function nrds_theme_settings_display_section() {
-    echo '<p>' . __('Manage display settings for the NRDS theme.', 'nrds-theme') . '</p>';
-}
-
-function nrds_theme_settings_general_section() {
-    echo '<p>' . __('Manage general settings for the NRDS theme.', 'nrds-theme') . '</p>';
-}
-
-function nrds_theme_setting_example_callback() {
-    $options = get_option('nrds_theme_settings_options');
-    ?>
-    <input type="text" name="nrds_theme_settings_options[nrds_theme_setting_example]" value="<?php echo isset($options['nrds_theme_setting_example']) ? esc_attr($options['nrds_theme_setting_example']) : ''; ?>" />
-    <p class="description"><?php _e('This is an example setting field.', 'nrds-theme'); ?></p>
-    <?php
-}
-
-function nrds_theme_settings_page() {
-    // Check user capabilities
-    if (!current_user_can('manage_options')) {
-        return;
-    }  
-    // Page content
-    echo '<div class="wrap">';
-    echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
-    echo '<p>Manage your theme\'s general settings here.</p>';
-    echo '<form method="post" action="options.php">';
-    settings_fields('nrds_theme_settings');
-    do_settings_sections('nrds-theme-settings');
-    submit_button();
-    
-    echo '</form>';
-    // You can add more sections and fields as needed
-    // For example, you can add a section for custom colors, fonts, etc.
-    // You can also add a section for custom CSS
-    // You can also add a section for custom JavaScript
-    // You can also add a section for custom header and background images
-    // You can also add a section for custom logo
-    // You can also add a section for custom menus
-    // You can also add a section for custom widgets
-    // You can also add a section for custom post types and taxonomies
-    // You can also add a section for custom shortcodes
-    // You can also add a section for custom page templates
-    // You can also add a section for custom theme options
-    // You can also add a section for custom theme settings
-    // You can also add a section for custom theme documentation
-    // You can also add a section for custom theme support
-    // You can also add a section for custom theme updates    
-    // Settings form can be added here
-    // For simplicity, we will just display a message       
-    echo '</div>';
-}
 
 ?>
