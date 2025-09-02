d0bb1c1a130ed64f4a67d3020204a93455f01cba
Jeremiah N0KMO <n0kmo@n0kmo.com>
2025-09-01 21:29:26 -0600

    Updated the comments and code formatting.

--- inc/theme-settings.php
+++ inc/theme-settings.php
@@ -82,7 +82,6 @@ function nrds_theme_settings_page() {
     if (!current_user_can('manage_options')) {
         return;
     }
-    // Page content
     ?>
     <div class="wrap">
     <h1><?php esc_html(get_admin_page_title())?></h1>
